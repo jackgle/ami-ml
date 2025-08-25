c0f7d9f2903021c57d48041261c104b226c9d188
jackgle <jackgl4124@gmail.com>
2025-08-25 16:33:04 +0800

    add .cls extension for sex label

--- src/dataset_tools/create_webdataset.py
+++ src/dataset_tools/create_webdataset.py
@@ -166,7 +166,7 @@ def _create_samples(
 
         # Add sex label if present
         if "sex" in row:
-            sample["sex"] = row["sex"]
+            sample["sexc.ls"] = row["sex"]
 
         if columns_to_json is not None:
             json_data = _prepare_json_data(row, columns_to_json)
